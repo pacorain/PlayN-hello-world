50982356592067b52a96bab19e566009f32a6d3b
Austin Paco DeArmond <hello@paco.audio>
2016-02-17 00:00:42 -0500

    Attempt to move everything

---
+++ core/src/main/java/audio/paco/playground/core/view/BoardView.java
@@ -0,0 +1,14 @@
+package audio.paco.playground.core.view;
+
+import playn.core.Surface;
+import playn.scene.Layer;
+
+/**
+ * Created by ajdearmond on 2/16/2016.
+ */
+public class BoardView extends Layer {
+    @Override
+    protected void paintImpl(Surface surf) {
+
+    }
+}
