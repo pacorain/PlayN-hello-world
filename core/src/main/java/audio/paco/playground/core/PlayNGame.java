4ff0ea5af22b08fe30bf88d99a15874e37f359e5
Austin Paco DeArmond <hello@paco.audio>
2016-02-19 08:55:38 -0500

    Implement BoardView
    Next task: Cleaning

--- core/src/main/java/audio/paco/playground/core/PlayNGame.java
+++ core/src/main/java/audio/paco/playground/core/PlayNGame.java
@@ -2,10 +2,14 @@ package audio.paco.playground.core;
 
 import audio.paco.playground.core.model.Coordinate;
 import audio.paco.playground.core.model.Piece;
+import audio.paco.playground.core.view.BoardView;
 import playn.core.Image;
 import playn.core.Platform;
+import playn.core.Surface;
 import playn.scene.ImageLayer;
+import playn.scene.Layer;
 import playn.scene.SceneGame;
+import pythagoras.f.IDimension;
 import react.RMap;
 import react.Value;
 
@@ -14,15 +18,37 @@ public class PlayNGame extends SceneGame {
     public static final int BOARD_SIZE = 8;
     public final RMap<Coordinate, Piece> board = RMap.create();
     public final Value<Piece> turn = Value.create(null);
+    private final IDimension size;
 
     public PlayNGame(Platform plat) {
-        super(plat, 33); // update our "simulation" 33ms (30 times per second)
+        super(plat, 33);
+        size = plat.graphics().viewSize;
+        addElements();
+    }
 
-        // create and add background image layer
-        Image bgImage = plat.assets().getImage("images/bg.png");
-        ImageLayer bgLayer = new ImageLayer(bgImage);
-        // scale the background to fill the screen
-        bgLayer.setSize(plat.graphics().viewSize);
-        rootLayer.add(bgLayer);
+    private void addElements() {
+        addBackground();
+        addBoard();
+    }
+
+    private void addBackground() {
+        rootLayer.add(new ColorLayer(0xFFCCCCCC));
+    }
+
+    private class ColorLayer extends Layer {
+        private final int color;
+
+        public ColorLayer(int color) {
+            this.color = color;
+        }
+
+        @Override
+        protected void paintImpl(Surface surface) {
+            surface.setFillColor(color).fillRect(0, 0, size.width(), size.height());
+        }
+    }
+
+    private void addBoard() {
+        rootLayer.addCenterAt(new BoardView(this, size), size.width() / 2, size.height() / 2);
     }
 }
